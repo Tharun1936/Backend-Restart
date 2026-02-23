46e33f69a99326db46e9cadedc11611e4a0a92ad
Tharun1936 <tharunmgowda1936@gmail.com>
2026-02-23 19:36:39 +0530

    rest complete

--- REST/index.js
+++ REST/index.js
@@ -1,8 +1,49 @@
 const express = require("express")
-
+const users = require("./MOCK_DATA.json")
 const app = express()
 const PORT = 3000
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
+
+app.get("/users", (req,res) =>{
+  const html = `
+  <ul>
+    ${users.map(user => `<li>${user.first_name} ${user.last_name}</li>`).join("")}
+  </ul>
+  `
+  res.send(html)
+})
+
+app.get("/api/users", (req, res) => {
+  res.json(users)
+})
+
+
+app.get("/api/users/:id", (req,res) =>{
+  const id = req.params.id
+  const user = users.find((user) =>user.id ===id )
+  return res.json(user)
+})
+
+
+app.post("/api/users", (req,res) =>{
+  const newUser = req.body
+  users.push(newUser)
+  res.status(201).json(newUser)
+})
+
+app 
+  .route("/api/users/:id")
+  .get((req,res) =>{
+    const id = req.params.id
+    const user = users.find((user) =>user.id ===id )
+    return res.json(user)
+    })
+  .patch((req,res) =>{
+    return res.json({status: "pending"})
+  })
+  .delete((req,res) =>{
+    return res.json({status: "pending"})
+  })
